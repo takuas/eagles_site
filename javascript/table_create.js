540251dfbd5538f8b5bb65b69608542cc2af7154
takuas <365.taku.as@gmail.com>
2023-10-29 00:20:22 +0900

    表を作成するJavascript再改良
    表のセルにクラスを付与して取得することで、特定の行の背景を全て変更するように改良

--- javascript/table_create.js
+++ javascript/table_create.js
@@ -5,8 +5,7 @@ function createTableHeader(table, headerLabels) {
   // 表の項目データを挿入
   headerLabels.forEach((labelText, index) => {
     const headerCell = document.createElement('td');
-
-    // 特定のセルにクラスとセルのサイズを指定する（コード番号55）
+    // 特定のセルにクラスとセルのサイズを指定する（コード番号62）
     cellClassGrant(headerCell,index)
 
     headerCell.textContent = labelText;
@@ -31,19 +30,27 @@ function createTableCell(table, data) {
       const cell = document.createElement('th');
       cell.textContent = cellData;
       
-      // 特定のセルにクラスとセルのサイズを指定する（コード番号55）
+      // 特定のセルにクラスとセルのサイズを指定する（コード番号62）
       cellClassGrant(cell, cellIndex)
       
       // 特定の行だけ背景を変更する為の条件式
       if (cellIndex === 1 && cellData === '楽天イーグルス') {
         rakuten = true;
       }
+
+      cell.classList.add('data_cell'); // セルにクラスを追加
+
       row.appendChild(cell);
     });
   
-    // 特定の行にクラスを追加し、背景を変更
+    // セルの要素を取得
+    const dataCell = row.getElementsByClassName('data_cell');
+
+    // 特定の行だけ背景の色を変化させる
     if (rakuten) {
-      row.classList.add('cell_background_color');
+      for (let i = 0; i < dataCell.length; i++) {
+        dataCell[i].classList.add('cell_rakuten_color'); // 新しいクラスを追加
+      }
     }
   
     // 表に挿入する
@@ -75,9 +82,9 @@ function createTable(data, headerLabels) {
   const table = document.createElement('table');
 
   // 表の項目を作成（コード番号2）
- createTableHeader(table, headerLabels);
+  createTableHeader(table, headerLabels);
   
-  // 表のデータを作成（コード番号23）
+  // 表のデータを作成（コード番号22）
   createTableCell(table, data)
 
   return table;
